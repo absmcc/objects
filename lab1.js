95ac98d83295cb9d05380ff1c4ba90967c30cbc5
Maria Redman <mariaredman@Marias-MacBook-Air-2.local>
2022-07-14 11:48:59 -0400

    Learned Objects

---
+++ lab1.js
@@ -0,0 +1,9 @@
+let person = {
+name: "Maria",
+age: 50,
+inClass: true
+}
+console.log(person)
+console.log("name")
+person.age = 25
+console.log(person.age)
---
+++ objects.js
@@ -0,0 +1,78 @@
+
+// let person 
+// name: 'Maria',
+// age: 50,
+// inClass: true,
+// classesTaught:['Javascript','arrays']
+// }
+// person.name = "Maria" //edit
+// person.hasGlasses = true //add a new property
+// console.log(person)
+
+// person.Age =21 //mistyped age (should be lowercase)
+// console.log(person)
+
+
+// let {name, age} =person
+// console.log(name)
+// console.log(age)
+
+// console.log(person)
+// console.log(person.name)
+// console.log(person.classesTaught[0])
+// console.log(person['classesTaught'][0])
+
+
+// person.name ="MChristine "
+// console.log(person.name)
+// console.log(person)
+// delete person.name
+// console.log(person.name)
+
+// 
+// let person = {
+// name: "Maria",
+// age: 50,
+// inClass: true
+// }
+// console.log(person)
+// console.log("name")
+// person.age = 25
+// console.log(person.age)
+
+
+// let teachers ={
+// name: "Mr. Todd",
+// classes: [],
+// currentlyTeaching: true
+// }
+
+// teachers.classes.push("Music", "PE") 
+// teachers.classes = "English"
+// // console.log("name")
+// // console.log(classes)
+// console.log(teachers) 
+
+let TeachersAssistants =[]
+let TeachersAssistant = {
+
+name: "Sarah",
+inClass: false,
+}
+
+TeachersAssistants.push(TeachersAssistants)
+console.log(TeachersAssistants)
+
+let TeachersAssistant2 = {
+Name: "Manny",
+inClass: false,
+    
+}
+TeachersAssistants.push(TeacherAssistant)
+console.log(TeachersAssistants[0].name)
+
+// console.log(TeachersAssistant)
+// TeachersAssistant.classes = []
+// TeachersAssistant.classes.push(["Java"])
+// console.log(TeachersAssistant)
+
